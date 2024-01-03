536b4270b5697396c2f5a4146d76488255c400c2
boyeon <qhdus96@naver.com>
2024-01-03 12:29:58 +0900

    Refactor/01: header refactoring
    - FixedBox atomic component 추가
    - Header 요소들 컴포넌트로 분리

--- boyeon-space/src/component/molecules/Header.tsx
+++ boyeon-space/src/component/molecules/Header.tsx
@@ -1,45 +1,39 @@
 /** @jsxImportSource @emotion/react */
-
+import { css } from "@emotion/react";
 import { useEffect, useState } from "react";
 import { maxq, minq } from "../../util/style.ts";
 import colors from "../atom/foundation/Color.tsx";
 import Icons from "../atom/Icons";
 import StyledTypography from "../atom/StyledTypography.tsx";
 import { Link } from "react-router-dom";
+import FlexBox from "../atom/FlexBox.tsx";
+import FixedBox from "../atom/FixedBox.tsx";
 
 const Header = () => {
   const [theme, setTheme] = useState("light");
-
   useEffect(() => {
     document.body.dataset.theme = theme;
   }, [theme]);
 
+
+  const headerStyle = {
+    height: css({ height: "90px" }),
+    container: css({
+      top: 0,
+      padding: "1rem 2rem",
+      backgroundColor: colors.primary1,
+      borderBottom: `2px solid ${colors.gray1}`,
+    }),
+  };
+
   return (
-    <header
-      css={{
-        height: "90px",
-      }}
-    >
-      <div
-        css={{
-          backgroundColor: colors.primary1,
-          position: "fixed",
-          top: 0,
-          width: "100%",
-          height: "90px",
-          borderBottom: `2px solid ${colors.gray1}`,
-          padding: "1rem 2rem",
-          boxSizing: "border-box",
-        }}
-      >
-        <div
-          className="header-container"
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
+    <header css={headerStyle.height}>
+      <FixedBox css={[headerStyle.height, headerStyle.container]}>
+        <FlexBox
+          direction="row"
+          align="center"
+          justify="space-between"
+          css={{ height: "100%" }}
         >
           <Icons
             name="menu"
@@ -47,68 +41,84 @@ const Header = () => {
             style={{
               [minq[1]]: {
                 display: "none",
-              }, 
-            }}
-          />
-
-          <div css={{ display: "flex" }}>
-            <StyledTypography variant="h0B">보연</StyledTypography>
-            <StyledTypography variant="h0" css={{lineHeight: '38px'}}>공간</StyledTypography>
-          </div>
-          {/* <img src="/src/assets/logo.png" css={{ height: "2rem" }} /> */}
-
-          <nav
-            css={{
-              [maxq[1]]: {
-                display: "none",
               },
             }}
-          >
-            <ul
-              css={{
-                display: "flex",
-                flexDirection: "row",
-                padding: 0,
-                li: {
-                  marginRight: "3rem",
-                },
-              }}
-            >
-              <li>
-                <Link to={"blog"}>
-                  <StyledTypography variant="h0">BLOG</StyledTypography>
-                </Link>
-              </li>
-              <li>
-                <Link to={"resume"}>
-                  <StyledTypography variant="h0">RESUME</StyledTypography>
-                </Link>
-              </li>
-              <li>
-                <Link to={"book"}>
-                  <StyledTypography variant="h0">BOOK</StyledTypography>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-
-          <div className="header-right">
-            <button
-              css={{
-                backgroundColor: "transparent",
-                borderColor: "transparent",
-              }}
-              onClick={() => setTheme("light" === theme ? "dark" : "light")}
-            >
-              <Icons
-                name={"light" === theme ? "clear_night" : "clear_day"}
-                size="large"
-              />
-            </button>
-          </div>
-        </div>
-      </div>
+          />
+          <Logo />
+          <NavBar />
+          <HeaderRight setTheme theme/>
+        </FlexBox>
+      </FixedBox>
     </header>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div>
+      <StyledTypography variant="h0B">보연</StyledTypography>
+      <StyledTypography variant="h0" css={{ lineHeight: "38px" }}>
+        공간
+      </StyledTypography>
+    </div>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <nav
+      css={{
+        [maxq[1]]: {
+          display: "none",
+        },
+      }}
+    >
+      <ul
+        css={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 0,
+          li: {
+            marginRight: "3rem",
+          },
+        }}
+      >
+        <li>
+          <Link to={"blog"}>
+            <StyledTypography variant="h0">BLOG</StyledTypography>
+          </Link>
+        </li>
+        <li>
+          <Link to={"resume"}>
+            <StyledTypography variant="h0">RESUME</StyledTypography>
+          </Link>
+        </li>
+        <li>
+          {/* <Link to={"book"}>
+          <StyledTypography variant="h0">BOOK</StyledTypography>
+        </Link> */}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const HeaderRight = ({setTheme, theme}: any) => {
+  return (
+    <div className="header-right">
+      <button
+        css={{
+          backgroundColor: "transparent",
+          borderColor: "transparent",
+        }}
+        onClick={() => setTheme("light" === theme ? "dark" : "light")}
+      >
+        <Icons
+          name={"light" === theme ? "clear_night" : "clear_day"}
+          size="large"
+        />
+      </button>
+    </div>
   );
 };
 
